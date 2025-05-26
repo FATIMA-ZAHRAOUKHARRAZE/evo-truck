8dba1b69a2fe98358c888d338eb9c4f35343e919
FATIMA-ZAHRAOUKHARRAZE <fatimazahraoukharraze@gmail.com>
2025-05-26 11:19:55 +0100

    update

--- resources/views/Solution.blade.php
+++ resources/views/Solution.blade.php
@@ -2,9 +2,8 @@
 @section('url')
     <link rel="stylesheet" href="{{ asset('css/solution.css') }}">
 @endsection
-@section('head')
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@section('seo')
+    <title>EVO MACHINERY - Solution</title>
     <meta name="description" content="EVO est une société multinationale avec la gamme la plus complète de produits concurrentiels et influents dans l'industrie des machines de construction.">
     <meta name="keywords" content="Evo, machinery, Bulldozer, Chargeuses, Niveleuses, Excavatrices, Grues mobiles, Grues à tour, Engins routiers, Machines à béton, Machines portuaires, Véhicules spéciaux, Machines d'assainissement, Machines pour les tunnels, Machines de battage de pieux, Équipement de travail aérien, Station de concassage et criblage, Matériel de lutte contre l'incendie, Machines d'exploration des ressources, Machines non destinées à l'excavation">
     <meta name="robots" content="index, follow">
@@ -12,6 +11,9 @@
     <meta name="language" content="French">
     <meta name="author" content="EVO MACHINERY">
     <link rel="canonical" href="https://evo-machinery.com/solution">
+    <meta property="og:title" content="EVO MACHINERY - Solution">
+    <meta property="og:description" content="EVO MACHINERY - Solution">
+    <meta property="og:type" content="website">
 @endsection
 @section('content')
     <!-- Page Image -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('layout.navbar')
 @section('seo')
+ <title>EVO MACHINERY</title>
     <meta name="description" content="EVO est une société multinationale avec la gamme la plus complète de produits concurrentiels et influents dans l'industrie des machines de construction.">
     <meta name="keywords" content="Evo, machinery, Bulldozer, Chargeuses, Niveleuses, Excavatrices, Grues mobiles, Grues à tour, Engins routiers, Machines à béton, Machines portuaires, Véhicules spéciaux, Machines d'assainissement, Machines pour les tunnels, Machines de battage de pieux, Équipement de travail aérien, Station de concassage et criblage, Matériel de lutte contre l'incendie, Machines d'exploration des ressources, Machines non destinées à l'excavation">
     <meta name="robots" content="index, follow">
@@ -7,8 +8,8 @@
     <meta name="language" content="French">
     <meta name="author" content="EVO MACHINERY">
     <link rel="canonical" href="https://evo-machinery.com/">
-    <meta property="og:title" content="EVO Machinery - Acceuil">
-    <meta property="og:description" content="Fabricant leader d'équipements de construction au Maroc">
+    <meta property="og:title" content="EVO MACHINERY - Acceuil">
+    <meta property="og:description" content="EVO MACHINERY - Acceuil">
     <meta property="og:type" content="website">
 @endsection
 @section('content')
