10309a2f50567ec16a69a387f36f9b22462c5071
FATIMA-ZAHRAOUKHARRAZE <fatimazahraoukharraze@gmail.com>
2025-05-27 09:37:05 +0100

    update1

--- resources/views/category/search.blade.php
+++ resources/views/category/search.blade.php
@@ -4,129 +4,111 @@
     <link rel="stylesheet" href="{{ asset('css/catliste.css') }}">
 @endsection
 @section('seo')
-    <<<<<<< HEAD <title>EVO MACHINERY - Chercher</title>
-        <meta name="description"
-            content="EVO est une société multinationale avec la gamme la plus complète de produits concurrentiels et influents dans l'industrie des machines de construction.">
-        <meta name="keywords"
-            content="Evo, machinery, Bulldozer, Chargeuses, Niveleuses, Excavatrices, Grues mobiles, Grues à tour, Engins routiers, Machines à béton, Machines portuaires, Véhicules spéciaux, Machines d'assainissement, Machines pour les tunnels, Machines de battage de pieux, Équipement de travail aérien, Station de concassage et criblage, Matériel de lutte contre l'incendie, Machines d'exploration des ressources, Machines non destinées à l'excavation">
-        =======
-        <title>EVO MACHINERY - Résultats de recherche pour "{{ $query }}"</title>
-        <meta name="description"
-            content="Trouvez les produits EVO Machinery correspondant à votre recherche '{{ $query }}'. Explorez notre catalogue d'équipements de construction et demandez un devis personnalisé.">
-        <meta name="keywords"
-            content="{{ $query }}, recherche produits, EVO Machinery, équipements de construction, machines industrielles, spécifications techniques, devis personnalisé, catalogue machines, qualité industrielle">
-        >>>>>>> 3c3a5f8e6e63dd66998fade1759ee1c3b3892ced
-        <meta name="robots" content="index, follow">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta name="author" content="EVO MACHINERY">
-        <link rel="canonical" href="https://evo-machinery.com/category/search?query={{ $query }}">
-        <meta property="og:title" content="EVO MACHINERY - Résultats pour '{{ $query }}'">
-        <meta property="og:description"
-            content="Découvrez les produits EVO Machinery correspondant à votre recherche '{{ $query }}'">
-        <meta property="og:type" content="website">
-        <meta property="og:image" content="{{ asset('images/Original.png') }}">
-        <meta name="search-query" content="{{ $query }}">
-    @endsection
-    @section('content')
-        <!-- Page Image -->
-        <div class="sss" style="position: relative; width: 100%;">
-            <img style="width: 100%; max-height: 400px; object-fit: cover; opacity: .4;" <<<<<<< HEAD
-                src="{{ asset('images/Original.png') }}" loading="lazy" alt="evo ">
-            =======
+    <title>EVO MACHINERY - Résultats de recherche pour "{{ $query }}"</title>
+    <meta name="description"
+        content="Trouvez les produits EVO Machinery correspondant à votre recherche '{{ $query }}'. Explorez notre catalogue d'équipements de construction et demandez un devis personnalisé.">
+    <meta name="keywords"
+        content="{{ $query }}, recherche produits, EVO Machinery,maroc, équipements de construction, machines industrielles, spécifications techniques, devis personnalisé, catalogue machines, qualité industrielle">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="author" content="EVO MACHINERY">
+    <link rel="canonical" href="https://evo-machinery.com/category/search?query={{ $query }}">
+    <meta property="og:title" content="EVO MACHINERY - Résultats pour '{{ $query }}'">
+    <meta property="og:description"
+        content="Découvrez les produits EVO Machinery correspondant à votre recherche '{{ $query }}'">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ asset('images/Original.png') }}">
+    <meta name="search-query" content="{{ $query }}">
+@endsection
+@section('content')
+    <!-- Page Image -->
+    <div class="sss" style="position: relative; width: 100%;">
+        <img style="width: 100%; max-height: 400px; object-fit: cover; opacity: .4;"
             src="{{ asset('images/Original.png') }}" loading="lazy" alt="">
-            >>>>>>> 3c3a5f8e6e63dd66998fade1759ee1c3b3892ced
-            <div
-                style="font-weight: bold; position: absolute; top: 20%; left:40%;  color: #2042be; font-size: 24px; padding: 10px;">
-                <span style="font-size: 50px;"> {{ translate('PRODUITS') }} </span>
-            </div>
+        <div
+            style="font-weight: bold; position: absolute; top: 20%; left:40%;  color: #2042be; font-size: 24px; padding: 10px;">
+            <span style="font-size: 50px;"> {{ translate('PRODUITS') }} </span>
         </div>
 
-        <div class="container">
-            @if ($products->isEmpty())
-                <p> {{ translate('Aucun produit trouvé.') }} </p>
-            @else
-                <div class="col-md-12 col-12">
-                    <div class="flex-wrap row d-flex">
-                        @foreach ($filteredDetails as $item)
-                            <div class="mb-4 product-item col-lg-3 col-md-6 col-sm-12" data-aos="fade-up">
-                                <div class="bbb_deals">
-                                    <div class="bbb_deals_slider_container">
-                                        <div class="bbb_deals_item" data-aos="zoom-in">
-                                            <div class="bbb_deals_image">
-                                                <a class="prodct-img-link"
-                                                    href="{{ route('ProductDetail', $item['product']->id) }}">
-                                                    <img src="{{ asset('images/' . $item['product']->img_pro) }}" <<<<<<<
-                                                        HEAD alt="evo ">
-                                                    =======
+    <div class="container">
+        @if ($products->isEmpty())
+            <p> {{ translate('Aucun produit trouvé.') }} </p>
+        @else
+            <div class="col-md-12 col-12">
+                <div class="flex-wrap row d-flex">
+                    @foreach ($filteredDetails as $item)
+                        <div class="mb-4 product-item col-lg-3 col-md-6 col-sm-12" data-aos="fade-up">
+                            <div class="bbb_deals">
+                                <div class="bbb_deals_slider_container">
+                                    <div class="bbb_deals_item" data-aos="zoom-in">
+                                        <div class="bbb_deals_image">
+                                            <a class="prodct-img-link"
+                                                href="{{ route('ProductDetail', $item['product']->id) }}">
+                                                <img src="{{ asset('images/' . $item['product']->img_pro) }}"
                                                     alt="">
-                                                    >>>>>>> 3c3a5f8e6e63dd66998fade1759ee1c3b3892ced
-                                                </a>
+                                            </a>
+                                        </div>
+                                        <div class="mb-1 bbb_deals_content">
+                                            <div class="flex-row bbb_deals_info_line d-flex justify-content-between">
+                                                <div class="mb-5 bbb_deals_item_name"
+                                                    style="color: #031c77;font-size: {{ strlen($item['product']->nom_pro) < 12
+                                                        ? '30px'
+                                                        : (strlen($item['product']->nom_pro) < 20
+                                                            ? '20px'
+                                                            : (strlen($item['product']->nom_pro) < 30
+                                                                ? '16px'
+                                                                : '14px')) }}">
+                                                    <b>{{ $item['product']->nom_pro }}</b>
+                                                </div>
                                             </div>
-                                            <div class="mb-1 bbb_deals_content">
-                                                <div class="flex-row bbb_deals_info_line d-flex justify-content-between">
-                                                    <div class="mb-5 bbb_deals_item_name"
-                                                        style="color: #031c77;font-size: {{ strlen($item['product']->nom_pro) < 12
-                                                            ? '30px'
-                                                            : (strlen($item['product']->nom_pro) < 20
-                                                                ? '20px'
-                                                                : (strlen($item['product']->nom_pro) < 30
-                                                                    ? '16px'
-                                                                    : '14px')) }}">
-                                                        <b>{{ $item['product']->nom_pro }}</b>
-                                                    </div>
-                                                </div>
-                                                @foreach ($item['details'] as $detail)
-                                                    <div class="bbb_deals_item_details" data-aos="fade-left">
-                                                        @foreach ($detail as $column => $value)
-                                                            <div class="bbb_deals_item_detail">
-                                                                <div class="bbb_deals_item_detail_label"
-                                                                    style="font-size: 13.7px">
-                                                                    {{ ucfirst(translate(Str::limit($column, 20))) }}
+                                            @foreach ($item['details'] as $detail)
+                                                <div class="bbb_deals_item_details" data-aos="fade-left">
+                                                    @foreach ($detail as $column => $value)
+                                                        <div class="bbb_deals_item_detail">
+                                                            <div class="bbb_deals_item_detail_label"
+                                                                style="font-size: 13.7px">
+                                                                {{ ucfirst(translate(Str::limit($column, 20))) }}
 
-                                                                </div>
-                                                                <div class="bbb_deals_item_detail_value">
-                                                                    {{ Str::limit($value, 6) }}
-
-                                                                </div>
                                                             </div>
-                                                        @endforeach
-                                                    </div>
-                                                @endforeach
-                                                <div class="mt-2 available">
-                                                    <div class="flex-row available_line d-flex justify-content-between">
-                                                        <a href="{{ route('ProductDetail', $item['product']->id) }}"
-                                                            class="btn" data-aos="flip-left">
-                                                            {{ translate('PLUS') }}
-                                                        </a>
-                                                        <a href="{{ route('order', [$item['product']->category_id, $item['product']->nom_pro]) }}"
-                                                            class="btn" data-aos="flip-right">
-                                                            {{ translate('LE DEVIS') }}
-                                                        </a>
-                                                    </div>
-                                                    <div class="available_bar"><span style="width:17%"></span></div>
+                                                            <div class="bbb_deals_item_detail_value">
+                                                                {{ Str::limit($value, 6) }}
+
+                                                            </div>
+                                                        </div>
+                                                    @endforeach
                                                 </div>
+                                            @endforeach
+                                            <div class="mt-2 available">
+                                                <div class="flex-row available_line d-flex justify-content-between">
+                                                    <a href="{{ route('ProductDetail', $item['product']->id) }}"
+                                                        class="btn" data-aos="flip-left">
+                                                        {{ translate('PLUS') }}
+                                                    </a>
+                                                    <a href="{{ route('order', [$item['product']->category_id, $item['product']->nom_pro]) }}"
+                                                        class="btn" data-aos="flip-right">
+                                                        {{ translate('LE DEVIS') }}
+                                                    </a>
+                                                </div>
+                                                <div class="available_bar"><span style="width:17%"></span></div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endforeach
-                    </div>
-
+                        </div>
+                    @endforeach
                 </div>
-            @endif
-        </div>
-        <div class="pagination-wrapper px-5">
-            {{ $products->appends(['query' => $query])->links('pagination::bootstrap-5') }}
-        </div>
 
-        <script>
-            // Select the <nav> element inside .pagination-wrapper
-            const navElement = document.querySelector('.pagination-wrapper nav');
+            </div>
+        @endif
+    </div>
+    <div class="pagination-wrapper px-5">
+        {{ $products->appends(['query' => $query])->links('pagination::bootstrap-5') }}
+    </div>
 
-            if (navElement) {
-                // Create a new <div> element
-                const divElement = document.createElement('div');
+    <script>
+        // Select the <nav> element inside .pagination-wrapper
+        const navElement = document.querySelector('.pagination-wrapper nav');
 
                 // Copy attributes from the <nav> to the <div>
                 Array.from(navElement.attributes).forEach(attr => {
